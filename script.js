05217a6eb20f48762ac334856508259d3f42f471
Sergey_Fedorov <109182055+Nemets87@users.noreply.github.com>
2025-02-14 22:11:33 +0300

    Update script.js
    5

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
-let phrases = [
-  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
-  { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
-  { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
-  { text: 'Arzamas', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
-  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
-  { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
-  { text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
-  { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
-  { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
-  { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
-  
-];
+// script.js
 
-function getRandomElement(arr) {
-  let randIndex = Math.floor(Math.random() * arr.length);
-  return arr[randIndex];
-}
+const svg = d3.select("#visualization")
+    .append("svg")
+    .attr("width", 800)
+    .attr("height", 600);
 
-let button = document.querySelector('.button');
-let phrase = document.querySelector('.phrase');
-let advice = document.querySelector('.advice');
-let image = document.querySelector('.image');
+d3.json("graph.json").then(data => {
+    const links = data.links;
+    const nodes = data.nodes;
 
-button.addEventListener('click', function () {
-  let randomElement = getRandomElement(phrases);
-  phrase.textContent = randomElement.text;
-  image.src = randomElement.image;
+    const link = svg.selectAll(".link")
+        .data(links)
+        .enter().append("line")
+        .attr("x1", d => nodes[d.source].x)
+        .attr("y1", d => nodes[d.source].y)
+        .attr("x2", d => nodes[d.target].x)
+        .attr("y2", d => nodes[d.target].y);
 
-  if (randomElement.text.length > 40) {
-    advice.style.fontSize = '33px';
-  } else {
-    advice.style.fontSize = '42px';
-  }
+    const node = svg.selectAll(".node")
+        .data(nodes)
+        .enter()
+        .append("g");
+
+    node.append("circle")
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("r", 20);
+
+    node.append("text")
+        .attr("dx", 12)
+        .attr("dy", ".35em")
+        .text(d => d.name);
+
+    // Добавляем названия файлов к узлам
+    const fileNames = {
+        "README.md": "README",
+        "cappy.img": "Карбочка",
+        "index.html": "index.html",
+        "script.js": "script.js",
+        "style.css": "style.css"
+    };
+
+    node.append("title")
+        .text(d => fileNames[d.name] || d.name);
+
+    // Добавляем эффект при наведении на узлы
+    node.on("mouseover", function() {
+        d3.select(this).select("circle").attr("fill", "#ffcc00");
+        d3.select(this).select("text").attr("font-weight", "bold");
+    })
+    .on("mouseout", function() {
+        d3.select(this).select("circle").attr("fill", "#000");
+        d3.select(this).select("text").attr("font-weight", "normal");
+    });
 });
